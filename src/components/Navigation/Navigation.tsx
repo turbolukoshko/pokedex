168ea3f7538205f24d21333136d3c23528ecf6d0
turbolukoshko <turbo.lukoshko@gmail.com>
2022-06-29 01:26:00 +0200

    added Navigation, PokemonList components, assets, styles

---
+++ src/components/Navigation/Navigation.tsx
@@ -0,0 +1,22 @@
+import logo from "../../assets/logo.png";
+import pokeball from "../../assets/pokeball2.png";
+import "./Navigation.scss";
+
+export const Navigation = () => {
+  return (
+    <nav className="nav">
+      <div className="nav__logo">
+        <img src={logo} alt="logo" />
+      </div>
+      <div className="nav__block">
+        <h1 className="nav__block-title">Who are Pokémon?</h1>
+        <p className="nav__block-description">
+          The good guide to explore the world of Pokemon
+        </p>
+        <div className="nav__block-image">
+          <img src={pokeball} alt="pokeball" />
+        </div>
+      </div>
+    </nav>
+  );
+};
---
+++ src/components/PokemonList/PokemonList.tsx
@@ -0,0 +1,3 @@
+import React from 'react';
+
+export const PokemonList = () => <main>Pokemon List</main>
